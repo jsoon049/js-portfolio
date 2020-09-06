fc7238b26e602ab92940fb1511e891a9076ae905
Jeremy <jsoon049@uottawa.ca>
2020-09-06 17:30:04 -0400

    Fixed resume publicURL issue

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,26 @@
 import React from "react"
 import Navbar from "./navbar.js"
 import Footer from "./footer.js"
-import resume from '/Users/jeremysoong/Desktop/js-portfolio/static/resume.pdf'  
-
-const navlinks = [
-  { name: "ABOUT", to: "#about" },
-  { name: "EXPERIENCE", to: "#experience" }, 
-  { name: "PROJECTS", to: "#projects" },
-  { name: "SKILLS", to: "#skills" },
-  { name: "CONTACT", to: "#contact" },
-  { name: "RESUME", to: resume }, 
-];
+import { useStaticQuery, graphql } from "gatsby"
  
 export default function Layout({ children }) {
+  const data = useStaticQuery(graphql`
+    {
+      pdf: file(name: { eq: "resume" }) {
+        publicURL
+      }
+    }
+  `)
+
+  const navlinks = [
+    { name: "ABOUT", to: "#about" },
+    { name: "EXPERIENCE", to: "#experience" }, 
+    { name: "PROJECTS", to: "#projects" },
+    { name: "SKILLS", to: "#skills" },
+    { name: "CONTACT", to: "#contact" },
+    { name: "RESUME", to: data.pdf.publicURL }, 
+  ];
+
   return (
     <div>
       <Navbar links={navlinks}/>
@@ -20,4 +28,4 @@ export default function Layout({ children }) {
       <Footer />
     </div>
   )
-}
+} 
